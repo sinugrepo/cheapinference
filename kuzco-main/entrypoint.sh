0cb4e7c8f42c342a9307ebafd8b0677e1dd4134b
Rizki Nugroho <54048440+sinugrepo@users.noreply.github.com>
2025-06-14 15:07:40 +0700

    Update entrypoint.sh

--- kuzco-main/entrypoint.sh
+++ kuzco-main/entrypoint.sh
@@ -8,55 +8,9 @@ if [ "$CODE" = "MISSING_CODE" ]; then
 fi
 
 # Jalankan main.py dulu
-echo "[BOOTSTRAP] Menjalankan backend"
+echo "[BOOTSTRAP] Menjalankan Backend"
 python3 main.py 2>&1 | tee /dev/stdout &
 
 # Start inference node (uvicorn) dan log ke stdout
 echo "[NODE] Memulai inference node dengan code: $CODE"
-inference node start --code "$CODE" 2>&1 | tee /dev/stdout &
-
-# Tunggu sampai port 14444 ready, lalu matikan dan jalankan nginx
-echo "[ENTRYPOINT] Menunggu setup inference node"
-(
-  while true; do
-      PID=$(lsof -i :14444 -sTCP:LISTEN -t)
-      if [ -n "$PID" ]; then
-          PROC=$(ps -p $PID -o comm=)
-          echo "[ENTRYPOINT] Port 14444 digunakan oleh: $PROC (PID $PID)"
-          
-          echo "[ENTRYPOINT] Membunuh proses di port 14444 (PID $PID)..."
-          kill "$PID"
-          sleep 1
-          
-          echo "[ENTRYPOINT] Menyalakan nginx..."
-          nginx
-          break
-      fi
-      sleep 1
-  done
-) 2>&1 | tee /dev/stdout &
-
-
-(
-  while true; do
-      PID=$(lsof -i :14444 -sTCP:LISTEN -t)
-      if [ -z "$PID" ]; then
-          echo "[ENTRYPOINT][WARNING] inference mati. Cek dan restart nginx..."
-
-          # Kill nginx jika masih ada instance sebelumnya
-          NGINX_PID=$(pidof nginx)
-          if [ -n "$NGINX_PID" ]; then
-              echo "[ENTRYPOINT] Membunuh proses nginx (PID $NGINX_PID)..."
-              kill $NGINX_PID
-              sleep 1
-          fi
-
-          echo "[ENTRYPOINT] Restart nginx..."
-          nginx
-      fi
-      sleep 5
-  done
-) 2>&1 | tee /dev/stdout &
-
-# Tahan container tetap hidup
-wait
+inference node start --code "$CODE" 2>&1 | tee /dev/stdout
